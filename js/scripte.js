8f188e0ccf14b9d14a4dbfedb8635b67bfe8d6cc
Enzo <EnzoPiccolo7>
2022-06-01 17:31:29 +0200

    primo commit

--- js/scripte.js
+++ js/scripte.js
@@ -1,7 +1,12 @@
+let fuoco = document.querySelector('footer img');
+let testo = document.querySelector('.container > div:nth-child(2)')
 function daifuoco(){
-    let fuoco = document.querySelector('footer img');
-    let testo = document.querySelector('.container > div:nth-child(2)')
     fuoco.classList.add('daifuoco');
     testo.classList.add('testo');
     alert('Ti ho detto di non cliccare, hai appena dato fuoco alla pagina!!!')
+}
+function spegni(){
+    fuoco.classList.remove('daifuoco');
+    testo.classList.remove('testo');
+    alert('bravo sei riuscito a spegnere l"incendio!!')
 }
