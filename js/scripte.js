a43673fcd94f20ca493a8d99e91a92ddf3ed0505
Enzo <EnzoPiccolo7>
2022-06-01 17:24:00 +0200

    primo commit

---
+++ js/scripte.js
@@ -0,0 +1,7 @@
+function daifuoco(){
+    let fuoco = document.querySelector('footer img');
+    let testo = document.querySelector('.container > div:nth-child(2)')
+    fuoco.classList.add('daifuoco');
+    testo.classList.add('testo');
+    alert('Ti ho detto di non cliccare, hai appena dato fuoco alla pagina!!!')
+}
